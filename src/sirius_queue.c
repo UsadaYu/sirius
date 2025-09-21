cc8c09dd5fe3722de0cea6f9673ff5913c0c7368
UsadaYu <2211213491a@gmail.com>
2025-09-21 17:01:26 +0800

    队列代码优化
    1. 修复无锁队列边界未判断问题；
    2. 优化队列计算速度

--- src/sirius_queue.c
+++ src/sirius_queue.c
@@ -12,7 +12,17 @@ typedef struct {
    */
   size_t *elements;
   size_t elem_nr;
+
+  /**
+   * @note It must be a power of 2.
+   */
   size_t capacity;
+  /**
+   * @brief Used for bit operations, it is equal to `capacity - 1`.
+   *
+   * @note This parameter is used for speed optimization calculation.
+   */
+  size_t capacity_mask;
 
   size_t front, rear;
 
@@ -45,6 +55,26 @@ typedef struct {
     } \
   } while (0)
 
+static inline size_t next_power_of_2(size_t n) {
+  if (n == 0)
+    return 2;
+  n--;
+  n |= n >> 1;
+  n |= n >> 2;
+  n |= n >> 4;
+  n |= n >> 8;
+  n |= n >> 16;
+#if __SIZEOF_SIZE_T__ == 8 // 64-bit
+  n |= n >> 32;
+#endif
+  n++;
+  return n;
+}
+
+static inline bool is_power_of_2(size_t n) {
+  return (n > 0) && ((n & (n - 1)) == 0);
+}
+
 sirius_api int sirius_que_alloc(sirius_que_t *cr, sirius_que_handle *handle) {
   if (unlikely(!cr || !handle)) {
     internal_error("Null pointer\n");
@@ -58,8 +88,20 @@ sirius_api int sirius_que_alloc(sirius_que_t *cr, sirius_que_handle *handle) {
     return sirius_err_memory_alloc;
   }
 
+  size_t requested_capacity = cr->elem_nr;
+  if (!is_power_of_2(requested_capacity)) {
+    q->capacity = next_power_of_2(requested_capacity);
+    internal_debg("Queue capacity adjusted from `%zu` to `%zu`\n",
+                  requested_capacity, q->capacity);
+  } else {
+    q->capacity = requested_capacity;
+  }
+
+  if (q->capacity == 0)
+    q->capacity = 2;
+
+  q->capacity_mask = q->capacity - 1;
   q->elem_nr = 0;
-  q->capacity = cr->elem_nr;
   q->front = 0;
   q->rear = 0;
   q->type = cr->que_type;
@@ -86,13 +128,10 @@ sirius_api int sirius_que_alloc(sirius_que_t *cr, sirius_que_handle *handle) {
 
 label_free4:
   sirius_cond_destroy(&q->cond_non_empty);
-
 label_free3:
   sirius_mutex_destroy(&q->mtx);
-
 label_free2:
   free(q->elements);
-
 label_free1:
   free(q);
 
@@ -122,22 +161,22 @@ sirius_api int sirius_que_free(sirius_que_handle handle) {
   return 0;
 }
 
-#define q_wait(ret, cond, que, w_nr, milliseconds) \
+#define q_wait(ret, cond, que, wait_nr, milliseconds) \
   { \
     ret = 0; \
-    if (w_nr == que->elem_nr) { \
+    if (wait_nr == que->elem_nr) { \
       switch (milliseconds) { \
       case sirius_timeout_none: \
         ret = sirius_err_timeout; \
         break; \
       case sirius_timeout_infinite: \
-        while (!ret && w_nr == que->elem_nr) { \
+        while (!ret && wait_nr == que->elem_nr) { \
           ret = sirius_cond_wait(&cond, &que->mtx); \
         } \
         break; \
       default: \
         ret = sirius_cond_timedwait(&cond, &que->mtx, milliseconds); \
-        ret = !ret && w_nr == que->elem_nr ? sirius_err_timeout : ret; \
+        ret = !ret && wait_nr == que->elem_nr ? sirius_err_timeout : ret; \
         break; \
       } \
     } \
@@ -155,13 +194,13 @@ sirius_api int sirius_que_free(sirius_que_handle handle) {
     switch (type) { \
     case sirius_que_type_mtx: \
       sirius_mutex_lock(&mtx); \
-      W if (!ret) { \
-        V sirius_cond_signal(&cond); \
+      W_B if (!ret) { \
+        C sirius_cond_signal(&cond); \
       } \
       sirius_mutex_unlock(&mtx); \
       break; \
     case sirius_que_type_no_mtx: \
-      V break; \
+      W_NB C break; \
     default: \
       internal_error("Invalid queue type: %d\n", type); \
       ret = sirius_err_args; \
@@ -179,18 +218,22 @@ sirius_api int sirius_que_get(sirius_que_handle handle, size_t *ptr,
   que_s *q = (que_s *)handle;
 
   int ret;
-#define V \
+#define C \
   *ptr = q->elements[q->front]; \
-  q->front = (q->front + 1) % q->capacity; \
-  (q->elem_nr)--;
-
-#define W q_wait(ret, q->cond_non_empty, q, 0, milliseconds)
+  q->front = (q->front + 1) & q->capacity_mask; \
+  q->elem_nr--;
+#define W_NB \
+  if (q->elem_nr == 0) { \
+    return sirius_err_resource_alloc; \
+  }
+#define W_B q_wait(ret, q->cond_non_empty, q, 0, milliseconds)
 
   q_tpl(ret, q->type, q->mtx, q->cond_non_full);
   return ret;
 
-#undef W
-#undef V
+#undef W_B
+#undef W_NB
+#undef C
 }
 
 sirius_api int sirius_que_put(sirius_que_handle handle, size_t ptr,
@@ -203,18 +246,22 @@ sirius_api int sirius_que_put(sirius_que_handle handle, size_t ptr,
   que_s *q = (que_s *)handle;
 
   int ret;
-#define V \
+#define C \
   q->elements[q->rear] = ptr; \
-  q->rear = (q->rear + 1) % q->capacity; \
-  (q->elem_nr)++;
-
-#define W q_wait(ret, q->cond_non_full, q, q->capacity, milliseconds)
+  q->rear = (q->rear + 1) & q->capacity_mask; \
+  q->elem_nr++;
+#define W_NB \
+  if (q->elem_nr == q->capacity) { \
+    return sirius_err_cache_overflow; \
+  }
+#define W_B q_wait(ret, q->cond_non_full, q, q->capacity, milliseconds)
 
   q_tpl(ret, q->type, q->mtx, q->cond_non_empty);
   return ret;
 
-#undef W
-#undef V
+#undef W_B
+#undef W_NB
+#undef C
 }
 
 sirius_api int sirius_que_reset(sirius_que_handle handle) {
